efc19649f48e05b0dcc1d4c07183e07f4987fdf6
odil-io <git@odil.io>
2022-10-03 09:51:21 +0200

    Update 404.php

--- site/404.php
+++ site/404.php
@@ -3,36 +3,33 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="Refresh" content="60; url=https://odil.io/" />
-    <title>404 Pagina niet gevonden</title>
+    <title>Pagina niet gevonden</title>
     <style media="screen">
-    html, body{
-      padding: 0px;
-      margin: 0px;
-      width: 100%;
-      height: 100%;
-      background-color: black;
-    }
-      video{
-        display: block;
-        width: 100%;
-        height:100%;
-        z-index: 10;
-      }
-      h1{
+    html,
+	body {
+		position: relative;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+		color: #292929;
+		background-color: #eaedf3;
+		height: 100vh;
+		width: 100vw;
+		margin: 0;
+		padding: 0;
+		text-size-adjust: 100%;
+		text-rendering: optimizeLegibility;
+		font-size: 1rem;
+	}
+    h1 {
         position:absolute;
-        top: 50px;
-        left: 0px;
-        right: 0px;
+        top: 50%;
+        left: 0;
+        right: 0;
         text-align: center;
-        color: white;
-        z-index: 999
+		transform: translateY(-50%);
       }
     </style>
   </head>
   <body>
-    <h1>Pagina niet gevonden!</h1>
-    <video autoplay loop>
-      <source src="/assets/video/404.mp4" type="video/mp4">
-    </video>
+    <h1>Tsja, da's prut, pagina fut!</h1>
   </body>
 </html>
