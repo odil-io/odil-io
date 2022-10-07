5ad92f0c23e15881196c16c6b8e476af4ac82d47
odil-io <git@odil.io>
2022-10-07 09:42:27 +0200

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -1,7 +1,34 @@
 <?php
 function api_status() {
-	if ( empty( json_decode( file_get_contents( __DIR__ . '/status.json' ) ) ) ) :
-		echo 'not ';
+	$file = __DIR__ . '/' . $filename;
+	if ( file_exists( $file ) ) :
+		echo 'online';
+	else:
+		echo 'offline';
 	endif;
-	echo 'available';
+}
+
+function pre( $filename ) {
+	$file = __DIR__ . '/' . $filename;
+	if ( file_exists( $file ) ) :
+		$contents = file_get_contents( $file );
+		$json     = json_decode( $contents );
+		if ( $json ) :
+			echo '<h1>' . $filename . '</h1>';
+			echo '<hr/>';
+			var_export( $json->action );
+			echo '<hr/>';
+			var_export( $json->workflow->id );
+			echo '<hr/>';
+			var_export( $json->workflow->name );
+			echo '<hr/>';
+			// var_export( $json->repository );
+			var_export( $json->sender->login );
+			echo '<hr/>';
+			var_export( $json->sender->avatar_url );
+			echo '<hr/>';
+			var_export( $json->sender->html_url );
+			echo '<hr/>';
+		endif;
+	endif;
 }
