753941e0a8c6d5ea5d1e3d53d48a901411ba6e84
odil-io <git@odil.io>
2022-10-07 09:42:33 +0200

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -1,18 +1,10 @@
 <?php
 // Recieve
 
-if ( $_REQUEST ) {
-	var_export( $_REQUEST );
-
-	$file    = __DIR__ . '/status.json';
-	$request = json_encode( $_REQUEST );
-	file_put_contents( $file, $content, FILE_APPEND | LOCK_EX );
-
-	if ( file_exists( $file ) ) {
-		echo 'Does exist.';
-	} else {
-		echo 'Does not exist.';
-	}
+if ( $_REQUEST && array_key_exists( 'payload', $_REQUEST ) ) {
+	$file    = __DIR__ . '/payload.json';
+	$request = $_REQUEST['payload'];
+	file_put_contents( $file, $request, FILE_APPEND );
 }
 
 // Authenticate
@@ -22,6 +14,23 @@ if ( $_REQUEST ) {
 	// echo hash( 'sha256', $_SERVER['HTTP_GITHUB_WEBHOOK_KEY'] );
 	// echo '<hr/>';
 	// echo hash( 'sha256', 'A3EFBC22D223F9A4AE6D45CBF77AA' );
+
+	// ["CONTENT_TYPE"]=>
+	// string(33) "application/x-www-form-urlencoded"
+	// ["HTTP_X_HUB_SIGNATURE_256"]=>
+	// string(71) "sha256=bd256e5813530e92ddeeefff5044fc53e8ad7df53c76f4cbcfd50bcc1c81410e"
+	// ["HTTP_X_HUB_SIGNATURE"]=>
+	// string(45) "sha1=3b1da9c4810e5d4f1e51ce8cb482e80e0e7c6956"
+	// ["HTTP_X_GITHUB_HOOK_INSTALLATION_TARGET_TYPE"]=>
+	// string(10) "repository"
+	// ["HTTP_X_GITHUB_HOOK_INSTALLATION_TARGET_ID"]=>
+	// string(9) "505789337"
+	// ["HTTP_X_GITHUB_HOOK_ID"]=>
+	// string(9) "382631835"
+	// ["HTTP_X_GITHUB_EVENT"]=>
+	// string(12) "workflow_run"
+	// ["HTTP_X_GITHUB_DELIVERY"]=>
+	// string(36) "4c133210-4575-11ed-9c58-44f4ec4c923e"
 
 
 // Verify
