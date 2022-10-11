603b510fa5491ba6ab15054c91ba15cd92dad504
odil-io <git@odil.io>
2022-10-11 10:30:20 +0200

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -1,34 +1,22 @@
 <?php
 function api_status() {
-	$file = __DIR__ . '/' . $filename;
-	if ( file_exists( $file ) ) :
+	$logfile = scandir( __DIR__ . '/entries' );
+	if ( is_array( $logfile ) ) {
 		echo 'online';
-	else:
+	}else{
 		echo 'offline';
-	endif;
+	}
 }
 
-function pre( $filename ) {
-	$file = __DIR__ . '/' . $filename;
-	if ( file_exists( $file ) ) :
-		$contents = file_get_contents( $file );
-		$json     = json_decode( $contents );
-		if ( $json ) :
-			echo '<h1>' . $filename . '</h1>';
-			echo '<hr/>';
-			var_export( $json->action );
-			echo '<hr/>';
-			var_export( $json->workflow->id );
-			echo '<hr/>';
-			var_export( $json->workflow->name );
-			echo '<hr/>';
-			// var_export( $json->repository );
-			var_export( $json->sender->login );
-			echo '<hr/>';
-			var_export( $json->sender->avatar_url );
-			echo '<hr/>';
-			var_export( $json->sender->html_url );
-			echo '<hr/>';
-		endif;
-	endif;
+function deliveries() {
+	$logfile = scandir( __DIR__ . '/entries' );
+	if ( $logfile ) {
+		foreach ( $logfile as $entryfile ) :
+			if ( ! in_array( $entryfile, array( '.', '..' ) ) ) :
+				echo '<div class="delivery">';
+					echo $entryfile;
+				echo '</div>';
+			endif;
+		endforeach;
+	}
 }
