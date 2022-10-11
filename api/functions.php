4080b1b1728641d738b156ca45c8b41009e5c02b
odil-io <git@odil.io>
2022-10-11 10:51:58 +0200

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -13,8 +13,10 @@ function deliveries() {
 	if ( $logfile ) {
 		foreach ( $logfile as $entryfile ) :
 			if ( ! in_array( $entryfile, array( '.', '..' ) ) ) :
+				$file = __DIR__ . '/entries/'. $entryfile;
+				$time = date ("H:i", filemtime($file));
 				echo '<div class="delivery">';
-					echo str_replace('.json', ' - ' . date (" H:i.", filemtime($entryfile)) ,$entryfile);
+					echo str_replace('.json', '<span>' . $time . '</span>' ,$entryfile);
 				echo '</div>';
 			endif;
 		endforeach;
