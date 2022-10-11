61c4c0a6464bad88e77ac38a33c1b17ee637e07c
odil-io <git@odil.io>
2022-10-11 10:38:20 +0200

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -1,9 +1,15 @@
 <?php
 if ( $_REQUEST && array_key_exists( 'payload', $_REQUEST ) ) {
-	$log    = __DIR__ . '/logs/' . $_SERVER['HTTP_X_GITHUB_DELIVERY'] . '.json';
+	$log    = __DIR__ . '/entries/' . $_SERVER['HTTP_X_GITHUB_DELIVERY'] . '.json';
 	$entry = array();
 	$entry['signature'] = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
 	$entry['payload']   = $_REQUEST['payload'];
-	file_put_contents( $log, json_encode( $entry ), FILE_APPEND );
+
+	try {
+		file_put_contents( $log, json_encode( $entry ), FILE_APPEND );
+		throw new Exception();
+	} catch (\Throwable $th) {
+		header("HTTP/1.1 500 Internal Server Error");
+	}
 }
 ?>
